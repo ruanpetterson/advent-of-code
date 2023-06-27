c1d57d7d977c84f0d3765f1c3175c5c1eb39b9ea
Ruan Petterson <ruan@petterson.eng.br>
2023-06-27 20:12:01 -0300

    feat: specialize `insert_by` implementation

--- advent-of-code-2022/src/bin/day-01/main.rs
+++ advent-of-code-2022/src/bin/day-01/main.rs
@@ -42,29 +42,64 @@ fn read_to_elves_iter<'i>(
 fn iter_to_ordered_list<const N: usize>(
     it: impl Iterator<Item = Elf>,
 ) -> [Elf; N] {
-    /// Inserts an element at position `index` within the slice, shifting all
-    /// elements after it to the right, overwriting the last one.
+    /// Inserts an item into a sorted slice at the appropriate position based on
+    /// a given predicate.
+    ///
+    /// The `insert_by` function takes a mutable reference to a slice, an item
+    /// to insert, and a predicate that compares elements of the slice with
+    /// the item. It finds the index in the slice where the item
+    /// should be inserted based on the ordering defined by the predicate and
+    /// shifts the elements after that index up by one position. The item is
+    /// then inserted at the determined index.
+    ///
+    /// If the slice is not sorted or if the comparator function does not
+    /// implement an order consistent with the sort order of the underlying
+    /// slice, the returned result is unspecified and meaningless.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use core::cmp::Ord;
+    /// let mut numbers = [1, 3, 5, 7, 9];
+    ///
+    /// insert_by(&mut numbers, 4, <i32 as Ord>::cmp);
+    ///
+    /// assert_eq!(numbers, [1, 3, 4, 5, 7]);
+    /// ```
     #[inline(always)]
-    fn try_insert_at<T>(slice: &mut [T], index: usize, element: T) {
-        if index < slice.len() {
-            // SAFETY: this is safe, trust me!
-            unsafe {
-                let p = slice.as_mut_ptr().add(index);
-                core::ptr::copy(p, p.add(1), slice.len() - index - 1);
-                p.write(element);
-            }
-        } else {
-            // Trying to insert out-of-bounds.
-            // No-op.
+    fn insert_by<T, F>(slice: &mut [T], element: T, predicate: F)
+    where
+        F: Fn(&T, &T) -> core::cmp::Ordering,
+    {
+        // Find the index that satisfies the predicate.
+        let Some(index) = slice.iter().position(|x| predicate(x, &element).is_ge()) else {
+            return; // No-op if predicate condition is not satisfied.
+        };
+
+        unsafe {
+            // SAFETY: This block of code is unsafe because it directly
+            // manipulates raw pointers and performs
+            // pointer arithmetic. However, it is considered safe under the
+            // following conditions:
+            // - The given slice is guaranteed to be valid, have proper
+            //   alignment, and contain properly initialized elements as Rust
+            //   guarantees these properties for slices.
+            // - The index is within the bounds of the slice length or equal to
+            //   the slice length. Therefore, the code does not access or modify
+            //   memory outside the bounds of the slice.
+
+            // Shift all elements after the index up one position
+            let ptr = slice.as_mut_ptr().add(index);
+            core::ptr::copy(ptr, ptr.add(1), slice.len() - index - 1);
+
+            // Insert the new element at the index
+            core::ptr::write(ptr, element);
         }
     }
 
     // Create an array to populate
     it.fold([Elf::default(); N], |mut list, elf| {
-        // Find the correct index to put the new Elf.
-        let (Ok(index) | Err(index)) =
-            list.binary_search_by(|i| i.cmp(&elf).reverse());
-        try_insert_at(list.as_mut_slice(), index, elf);
+        insert_by(&mut list, elf, |a, b| a.cmp(b).reverse());
         list
     })
 }
