0981acf2f6c0e44142855b70462832b582b0f610
Ruan Petterson <ruan@petterson.eng.br>
2025-06-20 12:44:56 -0300

    feat(2024): add day 01 part 2

--- advent-of-code-2024/src/bin/day-01/main.rs
+++ advent-of-code-2024/src/bin/day-01/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools as _;
 
 fn main() {
@@ -16,11 +18,17 @@ fn main() {
 
     println!("--- Day 1: Calorie Counting ---");
 
-    let total_distance = left
-        .iter()
-        .zip(right.iter())
-        .fold(0, |acc, (l, r)| (l - r).abs() + acc);
-    println!("What is the total distance between your lists? {total_distance}");
+    println!(
+        "What is the total distance between your lists? {total_distance}",
+        total_distance = total_distance(&left, &right)
+    );
+
+    println!("--- Part Two ---");
+    println!(
+        "Once again consider your left and right lists. What is their \
+         similarity score? {similarity}",
+        similarity = similarity(&left, &right)
+    );
 }
 
 fn pairs(input: &str) -> impl Iterator<Item = [i32; 2]> {
@@ -34,4 +42,34 @@ fn pairs(input: &str) -> impl Iterator<Item = [i32; 2]> {
         .map(|pair| {
             pair.map(|n| n.parse::<i32>().expect("BUG: expected number"))
         })
+}
+
+fn total_distance(left: &[i32], right: &[i32]) -> i32 {
+    left.iter()
+        .zip(right.iter())
+        .fold(0, |acc, (l, r)| (l - r).abs() + acc)
+}
+
+fn similarity(left: &[i32], right: &[i32]) -> i32 {
+    #[derive(Clone, Copy)]
+    struct Similarity {
+        count: i32,
+        value: i32,
+    }
+
+    let mut similarities: HashMap<i32, Similarity> = HashMap::new();
+    for i in left {
+        if let Some(sim) = similarities.get_mut(&i) {
+            sim.count += 1;
+            continue;
+        }
+
+        let value = right.iter().filter(|&j| i == j).copied().sum();
+
+        similarities.insert(*i, Similarity { count: 1i32, value });
+    }
+
+    similarities
+        .into_values()
+        .fold(0, |acc, Similarity { count, value }| acc + count * value)
 }
